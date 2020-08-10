2abb6160133669e73667914d7edaf316d3147607
chait3319 <chait@emblematicgroup.com>
2020-08-09 19:50:20 -0700

    License Addition
    Some cleanup

--- Assets/Normcore ParameterBinder/Scripts/Editor/PropertyBinderEditor.cs
+++ Assets/Normcore ParameterBinder/Scripts/Editor/PropertyBinderEditor.cs
@@ -1,3 +1,38 @@
+#region License
+//------------------------------------------------------------------------------ -
+// Normcore-ParameterBinder
+// https://github.com/chetu3319/Normcore-ParameterBinder
+//------------------------------------------------------------------------------ -
+// Original Author: Keijiro Takahashi
+// Gituhb Repo: https://github.com/keijiro/Lasp/blob/v2/Packages/jp.keijiro.lasp/Editor/PropertyBinderEditor.cs
+//------------------------------------------------------------------------------ -
+//
+// MIT License
+//
+// Copyright (c) 2020 Chaitanya Shah
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------ -
+
+#endregion
+
+
 using System;
 using UnityEngine;
 using UnityEditor;
@@ -39,7 +74,7 @@ sealed class PropertyBinderEditor
         {
            //  CreateNewPropertyBinderMenu(_datatype).DropDown(rect);
              //OnAddNewPropertyBinder<FloatValuePropertyBinder>();
-             CreateNewProprtyBinder(_datatype);
+             //CreateNewProprtyBinder(_datatype);
              switch (_datatype)        
              {
                  case "bool": 
