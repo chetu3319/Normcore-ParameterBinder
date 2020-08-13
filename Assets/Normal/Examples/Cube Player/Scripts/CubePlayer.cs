e953a25ceaee032e8f6cc49f5c16e3b9ac7df9bc
Andy <andy@radialgames.com>
2020-08-13 07:37:55 -0700

    update deprecated function call

--- Assets/Normal/Examples/Cube Player/Scripts/CubePlayer.cs
+++ Assets/Normal/Examples/Cube Player/Scripts/CubePlayer.cs
@@ -15,7 +15,7 @@ namespace Normal.Realtime.Examples {
 
         private void Update() {
             // If this CubePlayer prefab is not owned by this client, bail.
-            if (!_realtimeView.isOwnedLocally)
+            if (!_realtimeView.isOwnedLocallySelf)
                 return;
 
             // Make sure we own the transform so that RealtimeTransform knows to use this client's transform to synchronize remote clients.
